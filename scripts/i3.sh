eaa07036191df97465fe66e82261bab5ab57a7e2
cl51287 <cl51287@qq.com>
2019-08-31 19:36:32 +0800

    add oh-my-zsh

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo pacman -S xorg xorg-xinit i3-wm feh rofi compton
+sudo pacman -S xorg xorg-xinit i3-wm feh rofi compton zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
