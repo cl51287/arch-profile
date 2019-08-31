abb3ea957c2f1e5355fb27e105b7577d01579f9d
cl51287 <cl51287@qq.com>
2019-08-31 22:14:52 +0800

    fix path error

--- scripts/shadowsocks.sh
+++ scripts/shadowsocks.sh
@@ -2,8 +2,8 @@
 sudo pacman -S shadowsocks
 
 sudo mkdir -p /etc/shadowsocks
-sudo cp $(pwd)/shadowsocks/config.json /etc/shadowsocks
-sudo cp $(pwd)/shadowsocks/shadowsocks.service /etc/systemd/system/
+sudo cp $(dirname $0)/shadowsocks/config.json /etc/shadowsocks
+sudo cp $(dirname $0)/shadowsocks/shadowsocks.service /etc/systemd/system/
 
 sudo systemctl enable shadowsocks
 sudo systemctl start shadowsocks
