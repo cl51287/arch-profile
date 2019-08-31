0de83ad428c4c3a6cd17b2b97fa3cbc2e7a1348d
cl51287 <cl51287@qq.com>
2019-08-31 20:01:32 +0800

    add shadowsocks service

--- scripts/shadowsocks.sh
+++ scripts/shadowsocks.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 sudo pacman -S shadowsocks
+
+sudo mkdir -p /etc/shadowsocks
+sudo cp $(pwd)/shadowsocks/config.json /etc/shadowsocks
+sudo cp $(pwd)/shadowsocks/shadowsocks.service /etc/systemd/system/
+
 sudo systemctl enable shadowsocks
 sudo systemctl start shadowsocks
 
