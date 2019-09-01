0bd37a05f1bca6c5aadabfcbe24e5aa9f4d9ffe3
cl51287 <cl51287@qq.com>
2019-09-01 10:34:58 +0800

    add i3 status

--- scripts/i3.sh
+++ scripts/i3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo pacman -S xorg xorg-xinit i3-wm feh rofi compton zsh
+sudo pacman -S xorg xorg-xinit i3-wm feh rofi compton zsh i3status
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
